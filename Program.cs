d53d06f019930ec5bc38068533499cad8e2787b5
Vladislav Churilov <Nawaharjan@gmail.com>
2024-08-24 15:10:53 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            string input = Console.ReadLine();
+            if (input == "+")
+            {
+                Console.WriteLine(a + b);
+            }
+            else if (input == "-")
+            {
+                Console.WriteLine(a - b);
+            }
+            else if (input == "*")
+            {
+                Console.WriteLine(a * b);
+            }
+            else if (input == "/")
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("На ноль делить нельзя!");
+                }
+                else
+                {
+                    Console.WriteLine((double)a / b);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверная операция");
+            }
+        }
+    }
+}
